7cfc9ffb2bd343f992f7556d5c67b5001f1c9ad4
Caio Noronha <caionoronha@MacBook-Pro-de-Caio.local>
2026-02-27 21:27:16 -0300

    creating home package

---
+++ WalletPro/Home/Sources/Home/Home.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletPro
+//
+//  Created by Caio Noronha on 27/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
