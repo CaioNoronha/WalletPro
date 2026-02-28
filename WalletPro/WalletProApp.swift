d4beaacfbd79fd790564a602cef84f3c54262b4a
Caio Noronha <caionoronha@MacBook-Pro-de-Caio.local>
2026-02-27 21:16:23 -0300

    first commit

---
+++ WalletPro/WalletProApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletProApp.swift
+//  WalletPro
+//
+//  Created by Caio Noronha on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct WalletProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WalletProTests/WalletProTests.swift
@@ -0,0 +1,16 @@
+//
+//  WalletProTests.swift
+//  WalletProTests
+//
+//  Created by Caio Noronha on 27/02/26.
+//
+
+import Testing
+
+struct WalletProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
